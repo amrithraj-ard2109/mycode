524c670b0e7fa67bb179cd75ce48dfc0c53e22a8
amrithraj-ard2109 <64737845+amrithraj-ard2109@users.noreply.github.com>
2020-05-03 19:34:16 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,26 @@
+package week1.day1.assignments;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int n=8;
+		int n3=0;
+		int n1=0;
+		int n2=1;
+		
+		for (int i=1; i<=n; i++)
+		{
+			
+			System.out.println(n1);
+			n3=n1+n2;
+			n1=n2;
+			n2=n3;
+			
+			
+			
+			
+		}
+
+	}
+
+}
---
+++ PrimeNumber.java
@@ -0,0 +1,18 @@
+package week1.day1.assignments;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int prime=17;
+		if(prime%2!=0)
+		{
+			System.out.println(prime +" " + "is a prime number");
+		}
+		else 
+		{
+			System.out.println(prime +" " + "is not a prime number");
+		}
+
+	}
+
+}
